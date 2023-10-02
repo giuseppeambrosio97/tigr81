bbefac784a2465905cfc17056288d051f2d20478
Giuseppe Ambrosio <giuambro97@gmail.com>
2023-10-02 19:31:24 +0200

    [ADD] scaffold command

--- tigr81/tigr81/main.py
+++ tigr81/tigr81/main.py
@@ -1,19 +1,6 @@
 import typer
-
+from tigr81.commands import scaffold
 
 app = typer.Typer()
 
-
-@app.callback()
-def callback():
-    """
-    FAST API project scaffolder
-    """
-
-
-@app.command()
-def create_project_fast_api():
-    """
-    Scaffold a FAST API project
-    """
-    typer.echo("Loading portal gun")
+app.add_typer(scaffold.app(), name="scaffold")
