2cb753896ff218ed34bc4905de959b14b7a1a296
Giuseppe Ambrosio <giuambro97@gmail.com>
2024-05-01 20:40:46 +0200

    [FIX] tigr81: env var optional default None

--- tigr81/tigr81/__init__.py
+++ tigr81/tigr81/__init__.py
@@ -1,6 +1,6 @@
 import pathlib as pl
 
-__version__ = "1.1.0"
+__version__ = "1.1.1"
 
 PACKAGE_LOCATION = pl.Path(__file__).parent.resolve()
 ROOT_LOCATION = PACKAGE_LOCATION.parent
@@ -10,4 +10,5 @@ LOCAL_PROJECT_TEMPLATES_FOLDER_LOCATION = LOCAL_REPO_LOCATION / "project_templat
 RESOURCE_LOCATION = PACKAGE_LOCATION / "resources"
 
 
+PYPY_URL = "https://pypi.org/project/tigr81/"
 REPO_LOCATION = "https://github.com/giuseppeambrosio97/tigr81.git"
--- tigr81/tigr81/cli_settings.py
+++ tigr81/tigr81/cli_settings.py
@@ -1,8 +1,9 @@
+from typing import Optional
 from pydantic_settings import BaseSettings
 
 
 class CliSettings(BaseSettings):
-    tigr81_environment: str
+    tigr81_environment: Optional[str] = None
 
 
 CLI_SETTINGS = CliSettings()
