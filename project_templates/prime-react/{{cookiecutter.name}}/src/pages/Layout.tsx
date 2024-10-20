784292870f43cf0bbd534b9ef0879054c0cac4eb
Giuseppe Ambrosio <giuambro97@gmail.com>
2024-10-20 12:07:29 +0200

    perf(templates/prime-react): extract SidebarMenu

--- project_templates/prime-react/{{cookiecutter.name}}/src/pages/Layout.tsx
+++ project_templates/prime-react/{{cookiecutter.name}}/src/pages/Layout.tsx
@@ -1,65 +1,11 @@
-import { Outlet, useNavigate } from "react-router-dom";
-import { Sidebar } from "primereact/sidebar";
-import { Menu } from "primereact/menu";
-import { Button } from "primereact/button";
-import { useState } from "react";
-import { buttonDefaultPt } from "@/defaultPt";
+import { Outlet } from "react-router-dom";
+import SidebarMenu from "@/components/SidebarMenu";
 
 export default function Layout() {
-  const [visible, setVisible] = useState(false);
-  const navigate = useNavigate();
-
-  const menuItems = [
-    {
-      label: "Home",
-      items: [
-        {
-          label: "Home Page",
-          icon: "pi pi-home",
-          command: () => {
-            navigate("/home");
-          },
-        },
-        {
-          label: "POC Page",
-          icon: "pi pi-code",
-          command: () => {
-            navigate("/poc");
-          },
-        },
-        {
-          label: "Logout",
-          icon: "pi pi-sign-out",
-          command: () => {
-            navigate("/login");
-          },
-        },
-      ],
-    },
-  ];
-
   return (
     <>
-      {/* Sidebar with the Menu */}
-      <Sidebar visible={visible} onHide={() => setVisible(false)}>
-        <div className="flex flex-col gap-3">
-          <h2 className="text-center font-bold">Menu</h2>
-          {/* Menu component from PrimeReact */}
-          <Menu model={menuItems} className="w-full min-w-full" />
-        </div>
-      </Sidebar>
-
-      {/* Button to toggle the sidebar */}
-      <Button
-        icon="pi pi-bars"
-        size="large"
-        className="top-0 left-0 ml-2 mt-2"
-        {% raw %}
-        style={{ position: "fixed" }}
-        {% endraw %}
-        onClick={() => setVisible(true)}
-        pt={buttonDefaultPt}
-      />
+      {/* SidebarMenu component with the Sidebar and Menu */}
+      <SidebarMenu />
 
       {/* Header Section */}
       <header className="text-white flex justify-center font-bold text-2xl p-5 bg-primary mb-2">
