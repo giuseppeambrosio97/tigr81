d740171c41c46626c526ba87ce6df969f6aa7cfd
Giuseppe Ambrosio <giuambro97@gmail.com>
2024-10-24 11:48:22 +0200

    refactor(tigr81/hub): minor

--- tigr81/tigr81/commands/hub/helpers.py
+++ tigr81/tigr81/commands/hub/helpers.py
@@ -1,9 +1,10 @@
 import pathlib as pl
 from typing import Dict, List
 
+import typer
+
 from tigr81 import AVAILABLE_HUBS
 from tigr81.commands.hub.models import Hub, HubTemplate
-import typer
 
 
 def load_hubs(hub_folders: List[pl.Path] = AVAILABLE_HUBS) -> Dict[str, Hub]:
@@ -22,19 +23,18 @@ def is_hub_name_valid(
     return hub_name not in hubs
 
 
-def get_template_from_hubs(hub_name: str, template_name: str, hubs: Dict[str, Hub]) -> HubTemplate:
+def get_template_from_hubs(
+    hub_name: str, template_name: str, hubs: Dict[str, Hub]
+) -> HubTemplate:
     hub = hubs.get(hub_name)
     if not hub:
         typer.echo(f"Hub name not found: {hub_name}")
         raise typer.Exit()
-    
+
     template = hub.hub_templates.get(template_name)
 
     if not template:
         typer.echo(f"Hub template {template_name} not found in hub {hub_name}")
         raise typer.Exit()
-    
-    return template
-    
-    
 
+    return template
